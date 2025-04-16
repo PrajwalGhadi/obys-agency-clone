fa3f52badc0b2852001b77d598809bc5aa06a92e
PrajwalGhadi <prajwal.ghadi28@gmail.com>
2025-04-16 09:42:15 +0530

    Designing Part Complete

--- scripts.js
+++ scripts.js
@@ -1,32 +1,58 @@
-let tl = gsap.timeline();
+function loaderAnimation() {
+  let tl = gsap.timeline();
 
-tl.from('.line h1', {
+  tl.from(".line h1", {
     y: 200,
     stagger: 0.25,
     opacity: 0,
-})
+  });
 
-let growingCounter = document.querySelector('#growingCounter');
-let loadingLine = document.querySelector('#loading-line');
-let grow = 0;
+  let growingCounter = document.querySelector("#growingCounter");
+  let loadingLine = document.querySelector("#loading-line");
+  let grow = 0;
 
-setInterval(()=> {
+  setInterval(() => {
     if (grow < 100) {
-        growingCounter.innerHTML = grow++;
-        loadingLine.style.width = `${grow++ + 1}%`
+      growingCounter.innerHTML = grow++;
+      loadingLine.style.width = `${grow++ + 1}%`;
+    } else {
+      growingCounter.innerHTML = grow;
+      loadingLine.style.display = "none";
     }
-    else {
-        growingCounter.innerHTML = grow;
-        loadingLine.style.display = 'none';
-    }
-}, 30)
+  }, 30);
 
-tl.to('.loader', {
-    y:-1600,
-    opacity: 0,
+  tl.to(".loader", {
+    y: -1600,
+    display: "none",
     delay: 1,
+  });
+}
+loaderAnimation()
+
+let tl = gsap.timeline();
+tl.from('.centerText h1', {
+    y: 250,
+    stagger: 0.2,
+    delay: 2.5,
 })
 
-tl.to('main', {
-    y:-1600,
+tl.from('#hero-section-video', {
+  y:800,
+  delay: 1
+}) 
+
+document.addEventListener('mousemove', (dets) => {
+
+    gsap.from('#cursor', {
+        left: dets.x,
+        top: dets.y,
+        duration: 4,
+        ease: "power4.out",
+    })
+
+    Shery.makeMagnet("a" /* Element to target.*/, {
+      //Parameters are optional.
+      ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+      duration: 1,
+    });
 })
